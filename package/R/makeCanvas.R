413dd711d05030a24437c1c6da569a076b93e28c
Garrett <grolemund@gmail.com>
2011-07-11 17:03:16 +1200

    building canvas (graphical part) of vit tdisplay as an S4 object

--- package/R/makeCanvas.R
+++ package/R/makeCanvas.R
@@ -1,18 +1,68 @@
-# Master function for making the vit canvas
+# Master function for making the vit canvas. The vit canvas is an S4 object.
 
-makeCanvas <- function(data, text.box = FALSE){
-	.data <- as.data.frame(data)
-	.n <- nrow(.data)
-	.samples <- split(sample(1:n, n * 1000, replace = TRUE), rep(1:1000, each = n)),
-	.i <- 1 # sample counter
-	.stat <- median
+setOldClass("gTree")
+setOldClass("vpTree")
+
+setClass("canvas", representation(gTree= "gTree", viewports = "vpTree", 
+	data = "data.frame", samples = "list", which.sample = "numeric", n = "numeric"), 
+	prototype(gTree = gTree(), viewports = vpTree(viewport(), vpList()), 
+	data = data.frame(), samples = list(), which.sample = NA_real_, n = NA_real_))
+	
+	
+makeViewports <- function(data){
+	layout <- grid.layout(ncol = 2, widths = unit(c(.6, 1), "null"))
+	frameVP <- plotViewport(c(1,1,1,1), layout = layout, name = "canvas.frame")
+	text <- textBoxVP(layout.pos.col = 1)
+	graphs <- graphsBoxVP(data = data, layout.pos.col = 2) 
+	vpTree(frameVP, vpList(text, graphs))
+}
+
+
+makeVITgTree <- function(data, vpTree){
+	dataAxis <- xaxisGrob(name = "dataAxis", vp = graphsPath("data"))
+	sampleAxis <- xaxisGrob(name = "sampleAxis", vp = graphsPath("sample"))
+	statAxis <- xaxisGrob(name = "statAxis", vp = graphsPath("stat"))
+	
+	# move stacked to a stacked points grob that can calculate a conversion for points height in the viewport it pushes.
+	stacks <- data.frame(x = data[, 1], y = stackPoints(data[, 1]))
+	dataPoints <- pointsGrob(x = stacks$x, y = stacks$y, name = "dataPoints", 
+		vp = graphsPath("data"))
+	dataBox <- boxplotGrob(data[, 1], name = "dataBox", vp = graphsPath("data"))
+	
+	dataTextBox <- roundrectGrob(gp=gpar(fill = "#0000FF0D"), name = "dataTextBox", 
+		vp = vpPath("canvas.frame", "text", "text.data"))
+	sampleTextBox <- roundrectGrob(gp=gpar(fill = "#0000FF0D"), name = "sampleTextBox",
+		vp = vpPath("canvas.frame", "text", "text.sample"))
+	
+	# ADD DATA TEXT HERE
+	
+	gTree(name = "canvas", childrenvp = vpTree, children = gList(dataAxis, sampleAxis, 
+		statAxis, dataPoints, dataBox, dataTextBox, sampleTextBox))
+}
+	
+
+makeCanvas <- function(data){
+	data <- as.data.frame(data)
+	n <- nrow(data)
+	samples <- split(sample(1:n, n * 1000, replace = TRUE), rep(1:1000, each = n)),
+	vpt <- makeViewports()
+	gt <- makeVITgTree(data, )
+	
+	new("canvas", gTree = gt, viewports = vpt, data = data, samples = samples, 
+		which.sample = 1, n = n)
+}
+	
+
+
+
+
 	
 	# make viewports
 	if (text.box) {
 		layout <- grid.layout(ncol = 2, widths = unit(c(.6, 1), "null"))
 		frameVP <- plotViewport(c(1,1,1,1), layout = layout, name = "canvas.frame")
 		text <- textBoxVP(layout.pos.col = 1)
-		graphs <- graphsBoxVP(data = .data, layout.pos.col = 2) 
+		graphs <- graphsBoxVP(data = data, layout.pos.col = 2) 
 		vps <- vpTree(frameVP, vpList(text, graphs))
 	} else {
 		frameVP <- plotViewport(c(1,1,1,1), name = "canvas.frame")
