8eabae16d212e5134625a30e3befd4a6ce16a375
Garrett <grolemund@gmail.com>
2011-07-13 16:54:12 +1200

    cleaning up unused scripts

---
+++ working-drafts/makePlot.R
@@ -0,0 +1,12 @@
+# functions related to making a generic data plot
+
+#makeDataPlot <- function(data, layout = NULL, gp = NULL, vp = NULL, name = NULL){
+	
+#	dpgt <- plot1dGrob(data = data, layout = layout, name = name, gp = gp, vp = vp)
+	
+#	x <- data
+
+#	y <- stackPoints(data
+	
+#	points <- pointsGrob
+#}
--- working-drafts/stackPoints.R
+++ working-drafts/stackPoints.R
@@ -8,8 +8,22 @@
 #' @param nbins The number of bins to stack points into. nbins will be ignored if binwidth is specified
 #'
 #' @return a vector of y values. When x is plotted against stackPoints(x)
-stackPoints <- function(x, pheight = unit(1, "char"), xmin = min(x), xmax = max(x), ymin = 0, ymax = 1, 
+stackPoints <- function(x, y = c(0,1), pheight = unit(1, "char"), xmin = min(x), xmax = max(x), ymin = min(y), ymax = max(y), vp.path = current.vpPath(), 
 	binwidth = NULL, nbins = 30){
+		
+		
+		currentVP <- current.vpPath()
+		seekViewport(vp.path)
+		pheight <- convertY(pheight, "native", valueOnly = TRUE) * 0.8
+		
+		binwidth <- pheight * diff(range(x))
+		
+		nbins <- ceiling(diff(range(x) / binwidth))
+		breaks <- min(x) + c(0:(nbins)) * binwidth
+		group <- cut(x, breaks, include.lowest = TRUE)
+		max.stack <- max(table(group))
+		ydist <- min(1/max.stack, as.numeric(pheight))
+		
 		span <- xmax - xmin
 	
 		if (is.null(binwidth)) {
