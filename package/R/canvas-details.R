42cd9169cba9b3e66680f0fff53a40e0c3e398dc
Garrett <grolemund@gmail.com>
2011-07-25 15:54:16 +1200

    cleaning up files

---
+++ package/R/canvas-details.R
@@ -0,0 +1,11 @@
+plotProportionBars <-
+plotPoints <-
+plotTriangleDist <-
+plotCIStack <-
+plotArrow <-
+splitViewports <-
+plotTriangleDist <-
+yAxisViewports <- function() {
+	notYetImplemented()
+	stop("method does not exist yet.")
+}
