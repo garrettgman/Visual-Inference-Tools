058d58707b86e24377c8308491c40c9d364ed3a9
Ben Stevenson <bste085@aucklanduni.ac.nz>
2011-08-02 12:59:56 +1200

    added combobox widget for CI method. also added handlers to both stat and CI method combobox widgets to load CALC_STAT with the appropriate function from calc-stat-methods.R

--- package/R/vit-GUI.R
+++ package/R/vit-GUI.R
@@ -3,42 +3,53 @@
 
 vit <- function() {
 	e <- new.vit.env()
-	e$win <- gwindow("Visual Inference Tools", visible = TRUE, width = 870, 
+	e$win <- gwindow("Visual Inference Tools", visible = TRUE, width = 870,
     		height = 600)
-    
+
     # separates space into controls and plot
     g <- gpanedgroup(container = e$win, expand = TRUE)
     e$obj <- g
     g.controls <- gnotebook(container = g, expand = TRUE)
     g.plot <- ggraphics(container = g, expand = TRUE)
-    
+
     # control panel has a read data tab inspired from iNZight
     # and a VIT tab
-    controls.iNZight <- ggroup(horizontal = FALSE, container = g.controls, 
+    controls.iNZight <- ggroup(horizontal = FALSE, container = g.controls,
     	expand = TRUE, label = "Load Data")
-    controls.vit <- ggroup(horizontal = FALSE, container = g.controls, 
+    controls.vit <- ggroup(horizontal = FALSE, container = g.controls,
     	expand = TRUE, label = "Analyze Data")
-    	
-    	
+
+
     # adding vit controls
     addSpace(controls.vit, 10, horizontal = FALSE)
     tbl <- glayout(container = controls.vit)
 	tbl[1,1] <- glabel("Statistic:    ",container = tbl)
-	tbl[1,2] <- (e$stat <- gcombobox(c(), editable=TRUE, container = tbl, 
+	tbl[1,2] <- (e$stat <- gcombobox(c(), editable=TRUE, container = tbl,
 		handler = function(h, ...) {
-			if (!is.null(e$xData)) e$resetCanvas()
+                    if (svalue(e$stat) == "confidence interval")
+                        enabled(e$cimeth) = TRUE else enabled(e$cimeth) = FALSE
+                    loadStat(svalue(e$stat), svalue(e$cimeth))
+                    if (!is.null(e$xData)) e$resetCanvas()
 		}))
 	e$stat[] <- c("mean", "median", "confidence interval")
 	svalue(e$stat) <- "mean"
-	
+        tbl[2,1] <- glabel("CI Method:    ", container = tbl)
+        tbl[2,2] <- (e$cimeth <- gcombobox(c(), editable = TRUE, container = tbl,
+                                           handler = function(h, ...) {
+                                               loadStat(svalue(e$stat), svalue(e$cimeth))
+                                               if (!is.null(e$xData)) e$resetCanvas()
+                                           }))
+        e$cimeth[] <- c("normal", "percentile bootstrap", "normal bootstrap", "t bootstrap")
+        svalue(e$cimeth) <- "normal"
+	enabled(e$cimeth) <- FALSE
 	addSpace(controls.vit, 10, horizontal = FALSE)
 	vit.bootbox <- gframe("bootstrapping",  container = controls.vit)
 	e$redraw.radio <- gradio(c("1 (all)",1, 5, 20),  horizontal=FALSE)
 	add(vit.bootbox, e$redraw.radio)
-	
-	run1.but  <- gbutton(text = "Run", container = controls.vit, 
+
+	run1.but  <- gbutton(text = "Run", container = controls.vit,
 		handler = function(h, ...) {
-			n <- c("1 (all)" = 1, "1" = 1, "5" = 5, 
+			n <- c("1 (all)" = 1, "1" = 1, "5" = 5,
 				"20" = 20)[svalue(e$redraw.radio)]
 			for (i in 1:n) {
 				e$c1$plotSample(vp = graphsPath("sample"), name = "samplePlot")
@@ -48,55 +59,55 @@ vit <- function() {
 	point.but <- gbutton(text = "Track", container = controls.vit)
 	pause.but <- gbutton(text = "Pause", container = controls.vit)
 	addSpace(controls.vit, 40, horizontal=FALSE)
-	
-	vit.diffbox <- gframe("Capture Bootstrapping Difference", 
+
+	vit.diffbox <- gframe("Capture Bootstrapping Difference",
 		container = controls.vit)
-	bootstrap.radio <- gradio(c("1 (all)", 1, 5, 20, 1000),  
+	bootstrap.radio <- gradio(c("1 (all)", 1, 5, 20, 1000),
 		horizontal = FALSE)
 	add(vit.diffbox, bootstrap.radio)
-	
-	run2.but <- gbutton(text = "Run", container = controls.vit) 
-	show.ci.but <- gbutton(text = "Show Confidence Interval", 
-		container = controls.vit) 
+
+	run2.but <- gbutton(text = "Run", container = controls.vit)
+	show.ci.but <- gbutton(text = "Show Confidence Interval",
+		container = controls.vit)
 	addSpace(controls.vit, 10, horizontal = FALSE)
-  
-	status <- glabel("", container = controls.vit)    
-    
+
+	status <- glabel("", container = controls.vit)
+
 	#enabled(run1.but)      = FALSE
 	enabled(pause.but)     = FALSE
-	enabled(run2.but)      = FALSE  
+	enabled(run2.but)      = FALSE
 	enabled(show.ci.but)   = FALSE
 	enabled(point.but)     = FALSE
-	
-	
+
+
 	# adding iNZight controls
 	# top three buttons
 	iNZ.view <- ggroup()
 	e$importData <- gbutton("Import Data",
 		handler = function(h,...) e$fileReader())
-	e$dataView <- gbutton("View Data Set", 
+	e$dataView <- gbutton("View Data Set",
 		handler = function(h,...) e$viewData(h,...))
-	e$listView <- gbutton("View Variables", 
+	e$listView <- gbutton("View Variables",
 		handler = function(h,...) e$viewList(h,...))
-	font(e$importData) <- list(weight = "bold", family = "normal", 
+	font(e$importData) <- list(weight = "bold", family = "normal",
 		color = "navy")
-	font(e$dataView) <- list(weight = "bold", family = "normal", 
+	font(e$dataView) <- list(weight = "bold", family = "normal",
 		color = "navy")
-	font(e$listView) <- list(weight = "bold", family = "normal", 
+	font(e$listView) <- list(weight = "bold", family = "normal",
 		color = "navy")
 	add(iNZ.view, e$importData)
 	add(iNZ.view, e$dataView)
 	add(iNZ.view, e$listView)
 	add(controls.iNZight, iNZ.view)
-	
+
 	# Table of data frame or variables
 	e$dataGp <-  ggroup(horizontal = TRUE, expand = TRUE)
 	add(controls.iNZight, e$dataGp, expand = TRUE)
-	
+
 	# At start-up an (near)empty dataframe is embedded
 	tag(e$obj,"dataSet") = data.frame(empty = " ")
 	tag(e$obj,"originalDataSet") = data.frame(empty = " ")
-	e$dataSt <- gdf(tag(e$obj,"dataSet"), width = 20, height = 50, 
+	e$dataSt <- gdf(tag(e$obj,"dataSet"), width = 20, height = 50,
 		expand = TRUE)
 	add(e$dataGp, e$dataSt, expand = TRUE)
 	e$dataList1 <- NULL
@@ -108,18 +119,18 @@ vit <- function() {
 	tbl <- glayout(expand = FALSE)
 	tbl[3,1, anchor = c(0,0)] = glabel(" Variable 1 :")
 	tbl[5,1, anchor = c(0,0)] = glabel(" Variable 2 :")
-	
+
 	# column 2
 	e$xVar <- glabel("Drop name here")
 	font(e$xVar) <- list(weight = "bold", family = "normal")
 	tag(e$obj,"e$xVarData") <- NULL
 	tbl[3,3, anchor = c(0,0)] <- e$xVar
-	
+
 	e$yVar <- glabel("Drop name here")
 	font(e$yVar) <- list(weight = "bold", family = "normal")
 	tag(e$obj,"e$yVarData") <- NULL
 	tbl[5,3, anchor = c(0,0)] <- e$yVar
-	
+
 	# column 3
 	tbl[3,7, anchor = c(0,0)] <- gbutton("clear", handler = function(h,...) {
 		e$xData <- NULL
@@ -145,7 +156,7 @@ vit <- function() {
 		else e$xData <- tag(e$obj, "dataSet")[,id(h$dropdata)]
 		e$buildCanvas()
 	})
-	
+
 	adddroptarget(e$yVar, targetType = "object", handler = function(h, ...) {
 		svalue(e$yVar) <- id(h$dropdata)
 		if (e$inDataView) e$yData <- svalue(h$dropdata)
@@ -160,10 +171,10 @@ vit <- function() {
 ######################################################################
 ######################################################################
 ######################################################################
-######################################################################	
+######################################################################
 
 updateCanvas <- function() {
 	print("Coming Soon!")
 }
-	
-    
+
+
